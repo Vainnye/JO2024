c1bf04554d20de049b4c6711ca353c8850203ea6
Thomas Neuville <thomas.neuville@universite-paris-saclay.fr>
2024-05-22 18:10:49 +0200

    creation Pays

---
+++ src/modeles/Pays.java
@@ -0,0 +1,17 @@
+package modeles;
+
+public class Pays {
+	
+	private int numPays;
+	private String nomPays;
+	private int nbOr;
+	private int nbArgent;
+	private int nbBronze;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
