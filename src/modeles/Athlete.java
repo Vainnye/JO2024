c6816837f902cae2fb55f8f331b445aa58078b1b
Thomas Neuville <thomas.neuville@universite-paris-saclay.fr>
2024-05-22 18:09:48 +0200

    modif athlete

--- src/modeles/Athlete.java
+++ src/modeles/Athlete.java
@@ -1,7 +1,13 @@
 package modeles;
 
 public class Athlete {
+	
+	private int numAthlete;
+	private String prenomAthlete;
+	private String nomAthlete;
+	private String dateDeNaissance;
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
