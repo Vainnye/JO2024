5e105da923d82b7c888719a3c275061711f52fb1
Thomas Neuville <thomas.neuville@universite-paris-saclay.fr>
2024-05-24 15:29:58 +0200

    creation Compte

---
+++ src/modeles/Compte.java
@@ -0,0 +1,23 @@
+package modeles;
+
+public class Compte {
+	
+	private int numCompte;
+	private String nomUtilisateur;
+	private String prenomUtilisateur;
+	private String addressMailUtilisateur;
+	private String telephoneUtilisateur;
+	private String Apparence;
+	private boolean autorisationLocalisation;
+	private boolean autorisationNotification;
+	private String dateNaissanceUtilisateur;
+	private boolean gestionnaire;
+	private boolean administrateur;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
