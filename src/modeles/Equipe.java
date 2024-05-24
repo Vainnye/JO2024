20f51543e289afbc5684086a086a8a690837c1d0
Thomas Neuville <thomas.neuville@universite-paris-saclay.fr>
2024-05-24 15:24:28 +0200

    réparation

---
+++ src/modeles/Equipe.java
@@ -0,0 +1,15 @@
+package modeles;
+
+public class Equipe {
+	
+	int numEquipe;
+	int nbPersonnes;
+	String sexe;
+	String trancheAge;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
