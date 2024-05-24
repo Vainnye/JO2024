f8b963fac53a13d5c9419221798711de23979ab0
Thomas Neuville <thomas.neuville@universite-paris-saclay.fr>
2024-05-24 15:06:13 +0200

    creation Discipline

---
+++ src/modeles/Discipline.java
@@ -0,0 +1,13 @@
+package modeles;
+
+public class Discipline {
+	
+	int numDiscipline;
+	String nomDiscipline;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
