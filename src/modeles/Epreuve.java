57e5da8a8f4c7ff275149dac1a48e267a38c0b1a
Thomas Neuville <thomas.neuville@universite-paris-saclay.fr>
2024-05-24 15:01:03 +0200

    creation Epreuve

---
+++ src/modeles/Epreuve.java
@@ -0,0 +1,16 @@
+package modeles;
+
+public class Epreuve {
+	
+	int nmuEpreuve;
+	String dateEpreuve;
+	String deteDebut;
+	String dateFin;
+	float dureeHeure;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
