64b1153b85682cac2045750684550c8108b4613e
Thomas Neuville <thomas.neuville@universite-paris-saclay.fr>
2024-05-24 14:56:20 +0200

    creation Equipe

---
+++ src/modeles/equipe.java
@@ -0,0 +1,15 @@
+package modeles;
+
+public class equipe {
+	
+	int numEquipe;
+	int nbPersonnes;
+	String sexe;
+	String trancheAge;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
