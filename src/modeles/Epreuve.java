6491eea34b69c99c5dda95a6fcc32454ee841db6
Thomas Neuville <tomneuville91@gmail.com>
2024-05-25 12:57:14 +0200

    modif model

--- src/modeles/Epreuve.java
+++ src/modeles/Epreuve.java
@@ -2,26 +2,43 @@ package modeles;
 
 public class Epreuve {
 	
-	private int nmuEpreuve;
+	private int numEpreuve=0;
 	private String dateEpreuve;
-	private String deteDebut;
+	private String dateDebut;
 	private String dateFin;
 	private float dureeHeure;
+	private Discipline disciplineEpreuve;
 	
 	
 	
 	//----------------------------------------------------
 	//---------GETTERS------------------------------------
 	//----------------------------------------------------
-	public int getNmuEpreuve() {return nmuEpreuve;}
+	public int getNmuEpreuve() {return numEpreuve;}
 	public String getDateEpreuve() {return dateEpreuve;}
-	public String getDeteDebut() {return deteDebut;}
+	public String getDeteDebut() {return dateDebut;}
 	public String getDateFin() {return dateFin;}
 	public float getDureeHeure() {return dureeHeure;}
-
-
-
-
+	public Discipline getDisciplineEpreuve() {return disciplineEpreuve;}
+	
+	
+	
+	
+	
+	
+	public Epreuve(String dateEpreuve, String dateDebut, String dateFin, float dureeHeure,Discipline disciplineEpreuve) {
+		super();
+		this.dateEpreuve = dateEpreuve;
+		this.dateDebut = dateDebut;
+		this.dateFin = dateFin;
+		this.dureeHeure = dureeHeure;
+		this.disciplineEpreuve = disciplineEpreuve;
+		numEpreuve++;
+	}
+	
+	
+	
+	
 	public static void main(String[] args) {
 
 	}
