72d697cfdea8d874751b0e6d4840e71a4c324697
Vianney Jacquemot <vianney.jacquemot@universite-paris-saclay.fr>
2024-06-05 18:30:20 +0200

    disc

--- src/controleurs/CtrDiscipline.java
+++ src/controleurs/CtrDiscipline.java
@@ -1,11 +1,23 @@
 package controleurs;
 
+import vues.*;
+import modeles.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class CtrDiscipline {
 
+public class CtrDiscipline implements ActionListener {
+
+	vuesDiscipline vDiscipline;
+	Discipline mDiscipline;
+	
+	public CtrDiscipline (vuesDiscipline v, Discipline m) {
+		vDiscipline = v;
+		mDiscipline = m;
+	}
+	
 	public void actionPerformed(ActionEvent e) {
-		
+		//if ()
 	}
 	
 }
